82885c122a6b5bb5868b2505205b3ae5fece71f9
Christina Marvel <christinamarvel@gmail.com>
2022-07-07 16:02:05 -0600

    I'm having problems with gitbash not pushing into my repository so I had to upload my whole folder

---
+++ pypol.py
@@ -0,0 +1,12 @@
+import csv
+import os
+
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join ("Resources","election_results.csv")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+# Print the file object.
+   file_reader = csv.reader(election_data)
+   print(next(file_reader))
+    
